b3771a247e08c55085d62d5e2cfa908e33b47a31
mahsa abadian <abadian.ma@gmail.com>
2022-05-26 15:37:01 +0430

    edit-evaluation

---
+++ views/formValidation.js
@@ -0,0 +1,100 @@
+/*let userNameList = [];
+
+function getUsernameListFromDatabase(){
+    console.log('to check my function')
+
+    let xhr = new XMLHttpRequest();
+     xhr.onload = () => {
+     if (xhr.status >= 200 && xhr.status < 300){
+        const response = JSON.parse(xhr.response);
+        console.log('check response',xhr.response);
+        console.log(response);
+        userNameList = response;
+        console.log(userNameList,'helia');
+        return response
+   }
+
+ };
+ xhr.open('GET',"http://localhost:3000/api" ,true);
+ xhr.send();
+ 
+
+}
+if (userNameList && userNameList.find(username) ) {
+        say = " This password was registered before";
+        error_message.innerHTML = say;
+       return false;
+        
+
+
+
+console.log('mylist',userNameList);
+
+*/
+
+
+
+
+
+
+
+console.log('itis just a check')
+
+function formValidation() {
+    const btn = document.querySelector('button');
+    const username=document.getElementById('username').value;
+    const password=document.getElementById('password').value;
+    const password2=document.getElementById('password2').value;
+    let say;
+    console.log(password.length);
+    console.log(password);
+        
+
+    
+    if (username.length === 0) {
+        say = "Please enter a valid email address";
+        error_message.innerHTML = say;
+       return false;
+        
+    }
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(username).toLowerCase())){
+        say = "enter a vaild email";
+        error_message.innerHTML = say;
+       return false;
+
+    }
+    
+    if (password.length == 0 || password.length < 5 ) {
+        say = "Password must be at least 5 characters and must contain at least 1 capital letter"
+        error_message.innerHTML = say;
+       return false;
+        
+    }
+    
+    const  reg = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+    if (!reg.test(String(password).toLowerCase())){
+        say = "Password must be at least 5 characters and must contain at least 1 capital letter"
+        error_message.innerHTML = say;
+       return false;
+        
+    }
+
+    if (password !== password2) {
+        say = "Password does not match";
+        error_message.innerHTML = say;
+       return false;
+        
+    }
+    
+    alert("Form sent successfully")
+    
+    return true;
+
+}
+
+
+
+
+
+
